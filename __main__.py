be2ce9726cd43b5b9320f78210e31bb31a2bd1e5
Albrigs <natanfernandessantos@protonmail.com>
2021-03-08 21:15:16 -0300

    :fork_and_knife::heavy_plus_sign:lxml

--- __main__.py
+++ __main__.py
@@ -1,8 +1,12 @@
 from modules import *
 from argparse import ArgumentParser
+from pprint import pprint
 
 parser = ArgumentParser()
 
-parser.add_argument('search', help='Search package')
+parser.add_argument('-s', help='Search package')
 args = parser.parse_args()
-print(args)
+
+if args.s:
+	results = search_pkg(args.s)
+	pprint(results)
