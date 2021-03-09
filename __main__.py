dbd1029730a2ed11b34538b42f90c26edc80db67
Albrigs <natanfernandessantos@protonmail.com>
2021-03-08 21:09:34 -0300

    :fork_and_knife::heavy_plus_sign:Requeriments e argparser

--- __main__.py
+++ __main__.py
@@ -1,1 +1,8 @@
 from modules import *
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument('search', help='Search package')
+args = parser.parse_args()
+print(args)
